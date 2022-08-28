6a0b38de8d49bb8f97ec462f1af5e4e068a63e94
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-28 16:00:49 +0900

    feat: Define map, position structure (#2)

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 11:41:26 by sokim             #+#    #+#             */
-/*   Updated: 2022/08/28 13:50:10 by sokim            ###   ########.fr       */
+/*   Updated: 2022/08/28 15:57:58 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,44 @@
 # define CUB_3D_H
 
 # include "libft.h"
+# include "get_next_line.h"
 # include <stdio.h>
 # include <fcntl.h>
 
 # define FT_FALSE 0
 # define FT_TRUE 1
+# define FT_ERROR -1
+
+# define FT_EAST 0
+# define FT_WEST 1
+# define FT_SOUTH 2
+# define FT_NORTH 3
+
+typedef struct	s_position
+{
+	int		x;
+	int		y;
+	int		direction;
+}				t_position;
+
+typedef struct	s_map
+{
+	int			height;
+	int			width;
+	char		**map;
+	t_position	player;
+	int			num_of_player;
+}				t_map;
+
+/*
+ * Check functions
+ */
+void	check_map_validation(int fd, t_map *map);
+
+/*
+ * Exit functions
+ */
+void	exit_with_err_msg(char *msg, char **map);
+void	exit_with_fd_close(char *msg, char *line, int fd);
 
 #endif
