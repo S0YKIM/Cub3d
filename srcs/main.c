510b77c2bb09244ccdbca2dcdf04ec326c720a0b
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-29 13:17:53 +0900

    refactor: Combine fd and map into info structure

--- srcs/main.c
+++ srcs/main.c
@@ -6,34 +6,30 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 11:35:09 by sokim             #+#    #+#             */
-/*   Updated: 2022/08/29 12:01:50 by sokim            ###   ########.fr       */
+/*   Updated: 2022/08/29 13:12:48 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	open_file_name(char *path, t_info *info)
+static void	open_file_name(char *path, t_info *info)
 {
-	int		fd;
-
 	if (!path || ft_strcmp(path + ft_strlen(path) - 4, ".cub") != 0)
-		exit_with_free_all("Invalid file name.", NULL, NULL, FT_FALSE);
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
-		exit_with_free_all("Cannot find the file.", NULL, NULL, FT_FALSE);
-	check_map_validation(fd, info);
-	return (fd);
+		exit_with_free_all("Invalid file name.", NULL, info);
+	info->fd = open(path, O_RDONLY);
+	if (info->fd < 0)
+		exit_with_free_all("Cannot find the file.", NULL, info);
+	check_map_validation(info);
 }
 
 int		main(int argc, char **argv)
 {
-	int		fd;
 	t_info	info;
 
 	if (argc != 2)
-    exit_with_free_all("Invalid number of arguments.", NULL, NULL, FT_FALSE);
-	init_map(&info.map);
-	fd = open_file_name(argv[1], &info);
-	(void) fd;
+    exit_with_free_all("Invalid number of arguments.", NULL, &info);
+	init_info(&info);
+	open_file_name(argv[1], &info);
+	system("leaks cub3d");
 	return (0);
 }
