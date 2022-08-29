bcd5898bbb72e41abd8f1fe908b259948004b24b
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-29 12:12:11 +0900

    feat: Add function to malloc tex_files (#7)

--- srcs/init/init.c
+++ srcs/init/init.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 16:16:58 by sokim             #+#    #+#             */
-/*   Updated: 2022/08/28 16:19:35 by sokim            ###   ########.fr       */
+/*   Updated: 2022/08/29 11:50:11 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ void	init_map(t_map *map)
 	map->height = 0;
 	map->width = 0;
 	map->map = NULL;
-	map->player.x = -1;
-	map->player.y = -1;
-	map->player.direction = -1;
+	map->tex_files = (char **)ft_calloc(4, sizeof(char *));
+	if (!map->tex_files)
+		exit_with_free_all("Memory allocation failed.", NULL, NULL, 0);
+	map->floor = -1;
+	map->ceiling = -1;
 	map->num_of_player = 0;
 }
