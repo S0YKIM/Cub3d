5eae2c77da0380acb185d7129c1e333cd5809155
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-29 13:17:10 +0900

    feat: Implement init_info() and init_position() (#7)

--- srcs/init/init.c
+++ srcs/init/init.c
@@ -6,21 +6,35 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 16:16:58 by sokim             #+#    #+#             */
-/*   Updated: 2022/08/29 11:50:11 by sokim            ###   ########.fr       */
+/*   Updated: 2022/08/29 13:08:03 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	init_map(t_map *map)
+static void	init_position(t_position *player)
+{
+	player->x = -1;
+	player->y = -1;
+	player->direction = -1;
+}
+
+static void	init_map(t_info *info, t_map *map)
 {
 	map->height = 0;
 	map->width = 0;
 	map->map = NULL;
 	map->tex_files = (char **)ft_calloc(4, sizeof(char *));
 	if (!map->tex_files)
-		exit_with_free_all("Memory allocation failed.", NULL, NULL, 0);
+		exit_with_free_all("Memory allocation failed.", NULL, info);
 	map->floor = -1;
 	map->ceiling = -1;
 	map->num_of_player = 0;
+}
+
+void	init_info(t_info *info)
+{
+	info->fd = 0;
+	init_map(info, &info->map);
+	init_position(&info->player);
 }
