bd302995b5685033a747700c52fd389145b3ee06
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-29 13:16:23 +0900

    update: Include fd and map into info structure

--- srcs/exit/exit.c
+++ srcs/exit/exit.c
@@ -6,27 +6,27 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 15:55:11 by sokim             #+#    #+#             */
-/*   Updated: 2022/08/28 17:56:39 by sokim            ###   ########.fr       */
+/*   Updated: 2022/08/29 13:02:20 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	exit_with_free_all(char *msg, char **map, char *line, int fd)
+void	exit_with_free_all(char *msg, char *line, t_info *info)
 {
 	int	i;
 
-	if (map)
+	if (info->map.map)
 	{
 		i = 0;
-		while (map[i])
-			free(map[i]);
+		while (info->map.map[i])
+			free(info->map.map[i]);
 			i++;
 	}
 	if (line)
 		free(line);
-	if (fd)
-		close(fd);
+	if (info->fd)
+		close(info->fd);
 	ft_putendl_fd("Error", 2);
 	ft_putendl_fd(msg, 2);
 	exit(EXIT_FAILURE);
