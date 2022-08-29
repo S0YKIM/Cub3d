6f3576d3eaae3dcdeae4bf8d67a1ad2ba972842a
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-29 12:12:52 +0900

    refactor: Replace map into info structure

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 11:35:09 by sokim             #+#    #+#             */
-/*   Updated: 2022/08/28 18:12:38 by sokim            ###   ########.fr       */
+/*   Updated: 2022/08/29 12:01:50 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int		open_file_name(char *path, t_map *map)
+static int	open_file_name(char *path, t_info *info)
 {
 	int		fd;
 
@@ -21,18 +21,19 @@ int		open_file_name(char *path, t_map *map)
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
 		exit_with_free_all("Cannot find the file.", NULL, NULL, FT_FALSE);
-	check_map_validation(fd, &map);
+	check_map_validation(fd, info);
 	return (fd);
 }
 
 int		main(int argc, char **argv)
 {
 	int		fd;
-	t_map	map;
+	t_info	info;
 
 	if (argc != 2)
     exit_with_free_all("Invalid number of arguments.", NULL, NULL, FT_FALSE);
-	init_map(&map);
-	fd = open_file_name(argv[1]);
+	init_map(&info.map);
+	fd = open_file_name(argv[1], &info);
+	(void) fd;
 	return (0);
 }
