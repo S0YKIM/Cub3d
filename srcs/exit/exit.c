d8ea639ea59048584ca4519fbb7ad819c6d4c4bf
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-08-30 11:23:34 +0900

    update: Add null guard (#5)

--- srcs/exit/exit.c
+++ srcs/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 15:55:11 by sokim             #+#    #+#             */
-/*   Updated: 2022/08/29 15:42:36 by sokim            ###   ########.fr       */
+/*   Updated: 2022/08/30 11:16:29 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,22 @@ void	exit_with_free_all(char *msg, char *line, t_info *info)
 {
 	int	i;
 
-	if (info->map.map)
+	if (info && info->map.map)
 	{
 		i = 0;
 		while (info->map.map[i])
+		{
 			free(info->map.map[i]);
 			i++;
+		}
+		free(info->map.map);
 	}
 	if (line)
 		free(line);
-	if (info->fd)
+	if (info && info->fd)
 		close(info->fd);
 	i = 0;
-	while (info->map.tex_files[i])
+	while (info && info->map.tex_files && info->map.tex_files[i])
 		free(info->map.tex_files[i]);
 	ft_putendl_fd("Error", 2);
 	ft_putendl_fd(msg, 2);
