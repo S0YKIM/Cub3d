650fa74ee4b27e68008673de9a3234e4e4866399
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-09-05 16:02:34 +0900

    feat: Implement function to calculate start and end point to draw line (#45)

---
+++ srcs/dda/line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   line.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 15:21:10 by sokim             #+#    #+#             */
+/*   Updated: 2022/09/05 15:45:29 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	calc_line_height(int *start, int *end, t_dda *dda)
+{
+	int	line_height;
+
+	line_height = (int)(WINDOW_HEIGHT / dda->perp_wall_dist);
+	*start = WINDOW_HEIGHT / 2 - line_height / 2;
+	if (*start < 0)
+		*start = 0;
+	*end = line_height / 2 + WINDOW_HEIGHT / 2;
+	if (*end >= WINDOW_HEIGHT)
+		*end = WINDOW_HEIGHT - 1;
+}
